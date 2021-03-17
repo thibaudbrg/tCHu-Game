0919ca86d2aa95cc7e129e1a1bfc730bf0a8aabc
Matthieu Decotignie <matthieu.decotignie@epfl.ch>
2021-03-17 20:37:34 +0100

    C bon normalement cette semaine

--- test/ch/epfl/tchu/game/PlayerStateTest.java
+++ test/ch/epfl/tchu/game/PlayerStateTest.java
@@ -433,4 +433,106 @@ public class PlayerStateTest {
 
         assertEquals(8, playerstate.ticketPoints());
     }
+    @Test
+    void canClaimCardsWorks(){
+        //The SortedBag of the Cards
+        SortedBag.Builder<Card> cardBuilder = new SortedBag.Builder<>();
+        cardBuilder.add(3, Card.ORANGE)
+                .add(2, Card.LOCOMOTIVE)
+                .add(4,Card.BLUE);
+
+
+        //The List of the Routes
+        var s1 = new Station(0, "Lausanne");
+        var s2 = new Station(1, "EPFL");
+        var route1 = new Route("1", s1, s2, 1, Route.Level.OVERGROUND, Color.BLUE);
+
+        var s3 = new Station(2, "Zernez");
+        var s4 = new Station(3, "Klosters");
+        var route2 = new Route("2", s2, s3, 4, Route.Level.OVERGROUND, Color.ORANGE);
+
+        var s5 = new Station(4, "Lille");
+        var s6 = new Station(5, "Morteau");
+        var route3 = new Route("3", s5, s6, 3, Route.Level.OVERGROUND, Color.ORANGE);
+
+        var s7 = new Station(6, "Christian");
+        var s8 = new Station(7, "Grey");
+        var route4 = new Route("4", s7, s8, 1, Route.Level.OVERGROUND, Color.BLUE);
+
+        var s9 = new Station(8, "Karl");
+        var s10 = new Station(9, "Aberer");
+        var route5 = new Route("5", s2, s7, 4, Route.Level.UNDERGROUND, null);
+
+        var s11 = new Station(10, "Stalingrad");
+        var s12 = new Station(11, "LenineGrad");
+        var route6 = new Route("6", s11, s12, 4, Route.Level.OVERGROUND, Color.WHITE);
+
+        List<Route> routes = List.of(route1, route2, route3, route4, route5, route6);
+
+        //The SortedBad of the tickets
+        SortedBag.Builder<Ticket> ticketBuilder = new SortedBag.Builder<>();
+        Ticket ticket1 = new Ticket(s1, s3, 4);
+        Ticket ticket2 = new Ticket(s2, s5, 1);
+        Ticket ticket3 = new Ticket(s2, s7, 5);
+        ticketBuilder.add(ticket1)
+                .add(ticket2)
+                .add(ticket3);
+
+        //The PlayerState
+        PlayerState playerstate = new PlayerState(ticketBuilder.build(), cardBuilder.build(), routes);
+
+        assertFalse(playerstate.canClaimRoute(route2));
+        assertTrue(playerstate.canClaimRoute(route5));
+        assertTrue(playerstate.canClaimRoute(route3));
+        assertTrue(playerstate.canClaimRoute(route1));
+    }
+    @Test
+    void possibleClaimCardsFails(){
+        //The SortedBag of the Cards
+        SortedBag.Builder<Card> cardBuilder = new SortedBag.Builder<>();
+        cardBuilder.add(3, Card.ORANGE)
+                .add(2, Card.LOCOMOTIVE)
+                .add(Card.BLUE);
+
+
+        //The List of the Routes
+        var s1 = new Station(0, "Lausanne");
+        var s2 = new Station(1, "EPFL");
+        var route1 = new Route("1", s1, s2, 1, Route.Level.OVERGROUND, Color.BLACK);
+
+        var s3 = new Station(2, "Zernez");
+        var s4 = new Station(3, "Klosters");
+        var route2 = new Route("2", s2, s3, 4, Route.Level.UNDERGROUND, Color.ORANGE);
+
+        var s5 = new Station(4, "Lille");
+        var s6 = new Station(5, "Morteau");
+        var route3 = new Route("3", s5, s6, 4, Route.Level.OVERGROUND, Color.GREEN);
+
+        var s7 = new Station(6, "Christian");
+        var s8 = new Station(7, "Grey");
+        var route4 = new Route("4", s7, s8, 1, Route.Level.OVERGROUND, Color.BLUE);
+
+        var s9 = new Station(8, "Karl");
+        var s10 = new Station(9, "Aberer");
+        var route5 = new Route("5", s2, s7, 4, Route.Level.UNDERGROUND, null);
+
+        var s11 = new Station(10, "Stalingrad");
+        var s12 = new Station(11, "LenineGrad");
+        var route6 = new Route("6", s11, s12, 4, Route.Level.OVERGROUND, Color.WHITE);
+
+        List<Route> routes = List.of(route1, route2, route3, route4, route5, route6,route1,route2,route2,route2,route2,route2,route2,route2,route2,route2);
+
+        //The SortedBad of the tickets
+        SortedBag.Builder<Ticket> ticketBuilder = new SortedBag.Builder<>();
+        Ticket ticket1 = new Ticket(s1, s3, 4);
+        Ticket ticket2 = new Ticket(s2, s5, 1);
+        Ticket ticket3 = new Ticket(s2, s7, 5);
+        ticketBuilder.add(ticket1)
+                .add(ticket2)
+                .add(ticket3);
+
+        //The PlayerState
+        PlayerState playerstate = new PlayerState(ticketBuilder.build(), cardBuilder.build(), routes);
+        assertThrows(IllegalArgumentException.class,()->{playerstate.possibleClaimCards(route2);});
+    }
 }
