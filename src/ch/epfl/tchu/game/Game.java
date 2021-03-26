84ee0d80a6d1293a675af2a7d5a023d4d11436ea
Matthieu Decotignie <matthieu.decotignie@epfl.ch>
2021-03-26 14:20:21 +0100

    jnk

--- src/ch/epfl/tchu/game/Game.java
+++ src/ch/epfl/tchu/game/Game.java
@@ -2,8 +2,8 @@ package ch.epfl.tchu.game;
 
 import ch.epfl.tchu.Preconditions;
 import ch.epfl.tchu.SortedBag;
+import ch.epfl.tchu.gui.Info;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
@@ -12,12 +12,38 @@ public final class Game {
     static void play(Map<PlayerId, Player> players, Map<PlayerId, String> playerNames, SortedBag<Ticket> tickets, Random rng) {
         Preconditions.checkArgument(playerNames.size() == 2);
         Preconditions.checkArgument(players.size() == 2);
+        players.forEach((Id, p) -> {
+            p.initPlayers(Id, playerNames);
+        });
+        GameState gameState = GameState.initial(tickets, rng);
+        Game.sendInfoToBothPlayers(players, );
 
 
+        players.forEach((Id, p) -> {
+            p.setInitialTicketChoice(tickets);
+            p.chooseInitialTickets();
+        });
 
+        players.forEach((Id, p) -> {
+            sendInfoToBothPlayers(p, );
+        });
+
+
+        //The Game starts
+        gameState.
     }
 
-    //TODO receiveInfo
+    private static void sendInfoToBothPlayers(Map<PlayerId, Player> players, String s) {
+        players.forEach((Id, p) -> {
+            p.receiveInfo(s);
+        });
+    }
+
+    private static void updateStateBothPlayers(Map<PlayerId, Player> players, PublicGameState newState) {
+        players.forEach((Id, p) -> {
+            p.updateState(newState, );
+        });
+    }
     //TODO updateState
 
 }
